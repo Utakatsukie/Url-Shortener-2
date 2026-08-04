26283a4d12260d55daad4100398d851b71d7b9ee
Andrei Yatskov <AndreiYatskovJR@gmail.com>
2026-08-04 19:17:14 +0300

    feat: add basic login and register logic, begin work with config

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"Url-Shortener-2/internal/auth"
 	"Url-Shortener-2/internal/hello"
 	"fmt"
 	"net/http"
 )
 
 func main() {
+	//conf := configs.LoadConfig()
 	router := http.NewServeMux()
 	hello.NewHelloHandler(router)
+	auth.NewAuthHandler(router)
+	// /auth/login
+	// /auth/register
+
 	server := http.Server{
 		Addr:    ":8081",
 		Handler: router,
