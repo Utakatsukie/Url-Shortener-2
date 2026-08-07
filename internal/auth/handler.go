d933fab49205a302f7f2c9efd40ff12b0579531f
Andrei Yatskov <AndreiYatskovJR@gmail.com>
2026-08-07 17:35:31 +0300

    feat: add third version of email validation (go validator)

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -6,7 +6,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"net/mail"
+
+	"github.com/go-playground/validator/v10"
 )
 
 type AuthHandler struct {
@@ -28,25 +29,14 @@ func NewAuthHandler(router *http.ServeMux, deps AuthHandlerDeps) {
 func (handler *AuthHandler) Login() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		fmt.Println(handler.Config.Auth.Secret)
-		// read body
 		var payload LoginRequest
 		if err := json.NewDecoder(req.Body).Decode(&payload); err != nil {
 			res.Json(w, err.Error(), 402)
 			return
 		}
-		if payload.Email == "" {
-			res.Json(w, "Email required", 402)
-			return
-		}
-		//reg, _ := regexp.Compile(`[A-Za-z0-9\._%+\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,}`)
-		//ещё есть варианты проверки почты, видос пересмотри
-
-		if _, err := mail.ParseAddress(payload.Email); err != nil {
-			res.Json(w, "Wrong email", 402)
-			return
-		}
-		if payload.Password == "" {
-			res.Json(w, "Password required", 402)
+		validate := validator.New()
+		if err := validate.Struct(payload); err != nil {
+			res.Json(w, err.Error(), 402)
 			return
 		}
 		fmt.Println(payload)
