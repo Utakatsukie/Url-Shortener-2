ca347a9ff661344d23f52bea9e4587e3d8a3ee0b
Andrei Yatskov <AndreiYatskovJR@gmail.com>
2026-08-15 13:38:13 +0300

    feat: create link handler with CRUD functions

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"Url-Shortener-2/configs"
 	"Url-Shortener-2/internal/auth"
 	"Url-Shortener-2/internal/hello"
+	"Url-Shortener-2/internal/link"
 	"Url-Shortener-2/pkg/db"
 	"fmt"
 	"net/http"
@@ -13,12 +14,13 @@ func main() {
 	conf := configs.LoadConfig()
 	_ = db.NewDb(conf)
 	router := http.NewServeMux()
+
+	// Handler
 	hello.NewHelloHandler(router)
 	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
 		Config: conf,
 	})
-	// /auth/login
-	// /auth/register
+	link.NewLinkHandler(router, link.LinkHandlerDeps{})
 
 	server := http.Server{
 		Addr:    ":8081",
