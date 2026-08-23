6e26a272a8d5e623b05c6acdbc454cb5035b4c90
Andrei Yatskov <AndreiYatskovJR@gmail.com>
2026-08-23 15:22:04 +0300

    feat: add authorization middleware just for link update function

--- internal/link/handler.go
+++ internal/link/handler.go
@@ -1,6 +1,7 @@
 package link
 
 import (
+	"Url-Shortener-2/middleware"
 	"Url-Shortener-2/pkg/req"
 	"Url-Shortener-2/pkg/res"
 	"net/http"
@@ -22,7 +23,7 @@ func NewLinkHandler(router *http.ServeMux, deps LinkHandlerDeps) {
 		LinkRepository: deps.LinkRepository,
 	}
 	router.HandleFunc("POST /link", handler.Create())
-	router.HandleFunc("PATCH /link/{id}", handler.Update())
+	router.Handle("PATCH /link/{id}", middleware.IsAuthed(handler.Update()))
 	router.HandleFunc("DELETE /link/{id}", handler.Delete())
 	router.HandleFunc("GET /{hash}", handler.GoTo())
 }
