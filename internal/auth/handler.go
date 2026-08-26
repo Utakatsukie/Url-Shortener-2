c2f4e78466f4b1f51c77c780602e11195c77d54d
Andrei Yatskov <AndreiYatskovJR@gmail.com>
2026-08-26 21:52:05 +0300

    feat: integrated JWT tokens in register and login methods of authorization

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -2,9 +2,9 @@ package auth
 
 import (
 	"Url-Shortener-2/configs"
+	"Url-Shortener-2/pkg/jwt"
 	"Url-Shortener-2/pkg/req"
 	"Url-Shortener-2/pkg/res"
-	"fmt"
 	"net/http"
 )
 
@@ -34,9 +34,17 @@ func (handler *AuthHandler) Login() http.HandlerFunc {
 			return
 		}
 		email, err := handler.AuthService.Login(body.Email, body.Password)
-		fmt.Println(email, err)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		token, err := jwt.NewJWT(handler.Config.Auth.Secret).Create(email)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 		data := LoginResponse{
-			Token: "123",
+			Token: token,
 		}
 		res.Json(w, data, 200)
 	}
@@ -48,6 +56,19 @@ func (handler *AuthHandler) Register() http.HandlerFunc {
 		if err != nil {
 			return
 		}
-		handler.AuthService.Register(body.Email, body.Password, body.Name)
+		email, err := handler.AuthService.Register(body.Email, body.Password, body.Name)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		token, err := jwt.NewJWT(handler.Config.Auth.Secret).Create(email)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		data := RegisterResponse{
+			Token: token,
+		}
+		res.Json(w, data, 200)
 	}
 }
